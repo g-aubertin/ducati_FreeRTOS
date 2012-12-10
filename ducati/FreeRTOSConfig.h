3429b322edda5f8e8e028d4e30b4965a36f57807
Guillaume Aubertin <guillaume.aubertin@gmail.com>
2012-12-10 15:36:47 +0100

    increasing heap size

--- ducati/FreeRTOSConfig.h
+++ ducati/FreeRTOSConfig.h
@@ -72,7 +72,7 @@
 #define configCPU_CLOCK_HZ		( ( unsigned long ) 20000000 )
 #define configTICK_RATE_HZ		( ( portTickType ) 1000 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned short ) 70 )
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 7000 ) )
+#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 700000 ) )
 #define configMAX_TASK_NAME_LEN	( 10 )
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
